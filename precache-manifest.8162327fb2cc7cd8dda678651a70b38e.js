ffb284b9d29fc56e4e91607fecafc2cb8f516b06
shubh <ror.shubh@gmail.com>
2020-05-16 16:22:11 +0530

    Updates

---
+++ precache-manifest.8162327fb2cc7cd8dda678651a70b38e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6def606e9cbcb2554af7ccde817ed95",
+    "url": "/apertum-assignment/index.html"
+  },
+  {
+    "revision": "74f421f9bfa24622dd0b",
+    "url": "/apertum-assignment/static/css/main.a4b18d9f.chunk.css"
+  },
+  {
+    "revision": "c10b785641f30cff126a",
+    "url": "/apertum-assignment/static/js/2.03605e76.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/apertum-assignment/static/js/2.03605e76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74f421f9bfa24622dd0b",
+    "url": "/apertum-assignment/static/js/main.ea3e2483.chunk.js"
+  },
+  {
+    "revision": "3e279bc660b914425515",
+    "url": "/apertum-assignment/static/js/runtime-main.4b544869.js"
+  }
+]);
